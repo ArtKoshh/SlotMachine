3961278f3ced65d790b048f84735eb0c6ff92c2e
ArtKoshh <134922380+ArtKoshh@users.noreply.github.com>
2023-09-19 17:06:46 +0200

    Add files via upload

---
+++ Slotmachine/Slotmachine/BankAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slotmachine
+{
+    internal class BankAccount
+    {
+        public string Owner { get; }
+        public decimal Saldo { get; private set; }
+        public BankAccount(string owner, decimal initialSaldo)
+        {
+            Owner = owner;
+            Saldo = initialSaldo;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("");
+
+            Saldo += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount > Saldo)
+                throw new InvalidOperationException(
+                    "You don't have enough fund, you poor soul");
+
+            Saldo -= amount;
+        }
+        public void Transfer(decimal amount, BankAccount other)
+        {
+            Withdraw(amount);
+            other.Deposit(amount);
+        }
+
+    }
+
+}
---
+++ Slotmachine/Slotmachine/Program.cs
@@ -0,0 +1,55 @@
+﻿using Slotmachine;
+using System.Net.Security;
+using System.Transactions;
+
+var Costumer = new BankAccount("Pelle",1000);
+var Casino = new BankAccount("Casino", 300);
+
+Random rnd = new Random();
+int rNum = rnd.Next(1, 10);
+int WinNum = rnd.Next(1, 10);
+
+int WinningNum = 50;
+int CostPerRound = 25;
+bool Game=false;
+
+
+
+    Console.WriteLine("Hello Would you like to try your luck for 25 kr?PRESS ENTER");
+Console.ReadLine();
+    Costumer.Transfer(CostPerRound, Casino);
+    Console.WriteLine("Current number is {0} Your lucky number is {1}",rNum,WinNum);
+Console.WriteLine($"Costumer Saldo{Costumer.Saldo}Casino Saldo{Casino.Saldo}");
+
+
+
+
+
+{
+
+}
+if (rNum == WinNum)
+{
+    Console.WriteLine("Winner ! HERE is your price");
+    Casino.Transfer(WinningNum,Costumer);
+    
+}
+else if(rNum != WinningNum)
+{
+    Console.WriteLine("Sorry better luck next time, Would you like to try again? y = Yes,n = No");
+    string Choice = Console.ReadLine();
+    if (Choice == "y")
+    {
+        Costumer.Transfer(CostPerRound,Casino);
+        Console.WriteLine("Current number is {0} Your lucky number is {1}", rNum, WinNum);
+        Console.WriteLine($"Costumer Saldo{Costumer.Saldo}Casino Saldo{Casino.Saldo}");
+        return;
+
+    }
+    else
+    {
+        Console.WriteLine("Thank you for playing");
+        
+    }
+    
+}
